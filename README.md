0ca98872d8aa68b5d3446767e0a3471691062046
Luke Harding <lukeharding990@gmail.com>
2023-12-04 21:14:20 -0500

    File based vcard downloader

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ Designed to be ran so that when the link is opened in a mobile browser it displa
 The executable accepts the environment variable `BIND` to select which ip and which ports the server will respond to. (i.e. `BIND=0.0.0.0:3000 ./<executable>`)
   
 Defaults to `0.0.0.0:5000`
+  
+	
+The server will look for vcards in the `cards/` folder. So a file at `cards/lukeh990.vcf` will be translated to `http://BASEURL/lukeh990`.
+  
+  
+An API health check can be preformed at `GET /`
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,31 @@
-use rouille::Request;
-use rouille::Response;
+use rouille::{Response, router};
 use std::env;
+use std::fs::File;
 
 fn main() {
     let bind: String = match env::var("BIND") {
         Ok(port) => port,
-        Err(e) => "0.0.0.0:5000".to_string()
+        Err(_e) => "0.0.0.0:5000".to_string()
     };
 
     println!("Starting server on {}", bind);
 
     rouille::start_server(bind, move |request| {
-        Response::text("Hello World")
+        router!(request,
+            (GET) (/) => {
+                Response::text("I will survive")
+            },
+
+            (GET) (/{id: String}) => {
+                let file = match File::open(format!("cards/{}.vcf", id)) {
+                    Ok(file) => file,
+                    Err(_) => return Response::text("404 Not Found").with_status_code(404)
+                };
+
+                Response::from_file("text/x-vcard", file)
+            },
+
+            _ => Response::text("404 Not Found").with_status_code(404)
+        )
     });
 }
