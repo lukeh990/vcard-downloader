60a98110dab7e40bad7f7cea84e303fed0007ea5
Luke Harding <lukeharding990@gmail.com>
2024-01-07 15:53:50 -0500

    404 Page, begin decoupling card directory, and error handling

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,18 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-use rouille::{Response, router};
+use mime_guess;
+use rouille::{router, Response};
 use std::env;
 use std::fs::File;
-use mime_guess;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr, ToSocketAddrs};
 
 mod vcard;
 
 fn main() {
-    let bind: String = match env::var("BIND") {
-        Ok(port) => port,
-        Err(_e) => "0.0.0.0:5000".to_string()
-    };
+    let bind = get_bind_address();
 
-    println!("Starting server on {}", bind);
+    println!("Starting server on {}:{1}", bind.ip(), bind.port());
 
     rouille::start_server(bind, move |request| {
         router!(request,
@@ -30,7 +28,7 @@ fn main() {
                     return Response::from_file("text/html", file);
                 }
 
-                Response::text("404 Not Found").with_status_code(404)
+                not_found()
             },
 
             (GET) (/{id: String}) => {
@@ -42,15 +40,37 @@ fn main() {
                         }
                     };
                     return Response::from_file(mime_type, public);
-                } if let Ok(card) = File::open(format!("cards/{}.vcf", id)) {
+                } if let Ok(_card) = File::open(format!("cards/{}.vcf", id)) {
                     let data = vcard::create_vcard(id).to_string();
-                    return Response::from_data("text/x-vcard", data);
+                    return Response::from_data("text/vcard", data);
                 }
 
-                Response::text("404 Not Found").with_status_code(404)
+                not_found()
             },
 
-            _ => Response::text("404 Not Found").with_status_code(404)
+            _ => not_found()
         )
     });
+}
+
+fn get_bind_address() -> SocketAddr {
+    let environment = env::var("BIND").unwrap_or_default();
+
+    let env_socket = match environment.to_socket_addrs() {
+        Ok(mut socket_iter) => socket_iter.next(),
+        Err(_) => None,
+    };
+
+    match env_socket {
+        Some(socket) => return socket,
+        None => return SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 5000),
+    }
+}
+
+fn not_found() -> Response {
+    if let Ok(file) = File::open("public/404.html") {
+        return Response::from_file("text/html", file);
+    }
+
+    Response::text("404 Not Found").with_status_code(404)
 }
